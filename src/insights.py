0de0d5467a2f393bfb177ea9de64546a80dd967d
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-24 19:36:27 -0300

    Feat: build function to check if salary is in salary range

--- src/insights.py
+++ src/insights.py
@@ -174,7 +174,20 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if "min_salary" not in job:
+        raise ValueError
+    if "max_salary" not in job:
+        raise ValueError
+    elif type(job['min_salary']) != int:
+        raise ValueError
+    elif type(job['max_salary']) != int:
+        raise ValueError
+    elif type(salary) != int:
+        raise ValueError
+    elif job['min_salary'] > job['max_salary']:
+        raise ValueError
+    else:
+        return job['min_salary'] <= salary <= job['max_salary']
 
 
 def filter_by_salary_range(jobs, salary):
