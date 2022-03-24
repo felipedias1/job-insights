72f6a9639ce69fc7d00dc796b93ac7b6f29b599a
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-24 12:34:34 -0300

    Feat: build function to filter jobs by industry_type

--- src/insights.py
+++ src/insights.py
@@ -84,7 +84,11 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+    filtered_industry = []
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_industry.append(job)
+    return filtered_industry
 
 
 def get_max_salary(path):
