44ae0ceb064105bf33e43567643a48c215fa7269
Felipe Dias <felipeferreiradias1@gmail.com>
2022-03-24 11:34:10 -0300

    Feat: build function to filter jobs by job_type

--- src/insights.py
+++ src/insights.py
@@ -39,7 +39,11 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    filtered_jobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_jobs.append(job)
+    return filtered_jobs
 
 
 def get_unique_industries(path):
